0c946fa3f8dcfa7730ddbea0fc8afbe47bd127a8
Gabriel Etaat <60304519+Gabriel-Etaat@users.noreply.github.com>
2021-06-17 15:43:57 -0700

    Add files via upload

---
+++ Exercise Files/Ch2/variables_start.py
@@ -0,0 +1,24 @@
+# 
+# Example file for variables
+#
+
+# Declare a variable and initialize it
+f=0
+print(f)
+
+
+# re-declaring the variable works
+
+
+
+# ERROR: variables of different types cannot be combined
+
+
+
+# Global vs. local variables in functions
+
+
+# global makes f work outside the function throughout the script
+
+
+# del will delete a value as a function, so the print statement here wont work
